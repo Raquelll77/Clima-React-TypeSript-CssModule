17de940c480c4cc1ede7ec56f54ec5fc95caa5b9
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-30 16:58:31 -0600

    añadiendo al state los campos del formulario

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,23 @@
+import { useState, ChangeEvent } from "react";
+import { SearchType } from "../../types";
 import { countries } from "../../data/countries";
 import styles from './Form.module.css'
 
 
 export default function Form() {
+
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: ''
+  })
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+  }
+
   return (
     <form className={styles.form}>
         <div className={styles.field}>
@@ -12,12 +27,19 @@ export default function Form() {
                 id= "city"
                 name= "city"
                 placeholder="ciudad"
+                value={search.city}
+                onChange={handleChange}
             />
         </div>
 
         <div className={styles.field}>
-            <label htmlFor="pais">Pais:</label>
-            <select name="" id="">
+            <label htmlFor="country">Pais:</label>
+            <select 
+                name="country" 
+                id="country" 
+                value={search.country}
+                onChange={handleChange}
+            >
                 <option value="" disabled>-- Seleccione un pais --</option>
                 {countries.map(country => (
                     <option
