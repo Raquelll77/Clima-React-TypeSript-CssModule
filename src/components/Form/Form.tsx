68719a28fcec9b5c23ec24ccf81b24fac488af44
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-31 10:28:44 -0600

    haciendo consulta a la api de lat y long

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,7 +5,11 @@ import styles from './Form.module.css'
 import Alert from "../../Alert/Alert";
 
 
-export default function Form() {
+export type FormProps = {
+    fetchWeather: (search: SearchType) => Promise<void>
+}
+
+export default function Form({fetchWeather} : FormProps) {
 
   const [search, setSearch] = useState<SearchType>({
     city: '',
@@ -24,11 +28,11 @@ export default function Form() {
 
   const handleSumit  = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-
     if(Object.values(search).includes('')){
         setAlert('Todos los campos son obligatorios')
         return
     }
+    fetchWeather(search)
   }
 
   return (
